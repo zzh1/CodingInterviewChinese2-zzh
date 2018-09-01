24c809978c0409e2e88fe081cfc58c6538af2bfc
Zhonghao_Zhao <zhaozhonghao@simuwang.com>
2018-09-01 11:38:29 +0800

    18_02_DeleteDuplicatedNode

--- 18_02_DeleteDuplicatedNode/main.cpp
+++ 18_02_DeleteDuplicatedNode/main.cpp
@@ -104,9 +104,206 @@ void Test1()
     DestroyList(pHead);
 }
 
+//没有重复节点
+void Test2()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+    ListNode* pNode3 = CreateListNode(3);
+    ListNode* pNode4 = CreateListNode(4);
+    ListNode* pNode5 = CreateListNode(5);
+    ListNode* pNode6 = CreateListNode(6);
+    ListNode* pNode7 = CreateListNode(7);
+    
+    ConnectListNodes(pNode1,pNode2);
+    ConnectListNodes(pNode2,pNode3);
+    ConnectListNodes(pNode3,pNode4);
+    ConnectListNodes(pNode4,pNode5);
+    ConnectListNodes(pNode5,pNode6);
+    ConnectListNodes(pNode6,pNode7);
+    
+    ListNode* pHead = pNode1;
+    
+    int expectedValues[] = {1,2,3,4,5,6,7}; 
+    
+    Test("Test2",&pHead,expectedValues,sizeof(expectedValues)/sizeof(int));
+    
+    DestroyList(pHead);
+} 
+
+// 除了一个结点之外其他所有结点的值都相同
+void Test3()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(1);
+    ListNode* pNode3 = CreateListNode(1);
+    ListNode* pNode4 = CreateListNode(1);
+    ListNode* pNode5 = CreateListNode(1);
+    ListNode* pNode6 = CreateListNode(1);
+    ListNode* pNode7 = CreateListNode(2);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+    ConnectListNodes(pNode5, pNode6);
+    ConnectListNodes(pNode6, pNode7);
+
+    ListNode* pHead = pNode1;
+
+    int expectedValues[] = { 2 };
+    Test("Test3", &pHead, expectedValues, sizeof(expectedValues) / sizeof(int));
+
+    DestroyList(pHead);
+}
+
+// 所有结点的值都相同
+void Test4()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(1);
+    ListNode* pNode3 = CreateListNode(1);
+    ListNode* pNode4 = CreateListNode(1);
+    ListNode* pNode5 = CreateListNode(1);
+    ListNode* pNode6 = CreateListNode(1);
+    ListNode* pNode7 = CreateListNode(1);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+    ConnectListNodes(pNode5, pNode6);
+    ConnectListNodes(pNode6, pNode7);
+
+    ListNode* pHead = pNode1;
+
+    Test("Test4", &pHead, NULL, 0);
+
+    DestroyList(pHead);
+}
+
+// 所有结点都成对出现
+void Test5()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(1);
+    ListNode* pNode3 = CreateListNode(2);
+    ListNode* pNode4 = CreateListNode(2);
+    ListNode* pNode5 = CreateListNode(3);
+    ListNode* pNode6 = CreateListNode(3);
+    ListNode* pNode7 = CreateListNode(4);
+    ListNode* pNode8 = CreateListNode(4);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+    ConnectListNodes(pNode5, pNode6);
+    ConnectListNodes(pNode6, pNode7);
+    ConnectListNodes(pNode7, pNode8);
+
+    ListNode* pHead = pNode1;
+
+    Test("Test5", &pHead, NULL, 0);
+
+    DestroyList(pHead);
+}
+
+// 除了两个结点之外其他结点都成对出现
+void Test6()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(1);
+    ListNode* pNode3 = CreateListNode(2);
+    ListNode* pNode4 = CreateListNode(3);
+    ListNode* pNode5 = CreateListNode(3);
+    ListNode* pNode6 = CreateListNode(4);
+    ListNode* pNode7 = CreateListNode(5);
+    ListNode* pNode8 = CreateListNode(5);
+
+    ConnectListNodes(pNode1, pNode2);
+    ConnectListNodes(pNode2, pNode3);
+    ConnectListNodes(pNode3, pNode4);
+    ConnectListNodes(pNode4, pNode5);
+    ConnectListNodes(pNode5, pNode6);
+    ConnectListNodes(pNode6, pNode7);
+    ConnectListNodes(pNode7, pNode8);
+
+    ListNode* pHead = pNode1;
+
+    int expectedValues[] = { 2, 4 };
+    Test("Test6", &pHead, expectedValues, sizeof(expectedValues) / sizeof(int));
+
+    DestroyList(pHead);
+}
+
+// 链表中只有两个不重复的结点
+void Test7()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(2);
+
+    ConnectListNodes(pNode1, pNode2);
+
+    ListNode* pHead = pNode1;
+
+    int expectedValues[] = { 1, 2 };
+    Test("Test7", &pHead, expectedValues, sizeof(expectedValues) / sizeof(int));
+
+    DestroyList(pHead);
+}
+
+// 结点中只有一个结点
+void Test8()
+{
+    ListNode* pNode1 = CreateListNode(1);
+
+    ConnectListNodes(pNode1, NULL);
+
+    ListNode* pHead = pNode1;
+
+    int expectedValues[] = { 1 };
+    Test("Test8", &pHead, expectedValues, sizeof(expectedValues) / sizeof(int));
+
+    DestroyList(pHead);
+}
+
+// 结点中只有两个重复的结点
+void Test9()
+{
+    ListNode* pNode1 = CreateListNode(1);
+    ListNode* pNode2 = CreateListNode(1);
+
+    ConnectListNodes(pNode1, pNode2);
+
+    ListNode* pHead = pNode1;
+
+    Test("Test9", &pHead, NULL, 0);
+
+    DestroyList(pHead);
+}
+
+// 空链表
+void Test10()
+{
+    ListNode* pHead = NULL;
+
+    Test("Test10", &pHead, NULL, 0);
+}
+
 int main(int argc, char *argv[])
 {
     Test1();
+    Test2();
+    Test3();
+    Test4();
+    Test5();
+    Test6();
+    Test7();
+    Test8();
+    Test9();
+    Test10();
+    
     
     system("PAUSE");
     return EXIT_SUCCESS;
